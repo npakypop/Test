733dde1d94b8f99aae46ec63f1d3497545c41115
AntonKh <leaterood@gmail.com>
2022-12-27 09:05:58 +0300

    dlt comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,64 +1,8 @@
 import { Component } from 'react';
-// import { Card } from './Card/Card';
 import { CardList } from './CardList/CardList';
 import { Section } from './Section/Section.styled';
 import users from '../data/users.json';
 
-// export class App extends Component {
-//   state = {
-//     id: '1',
-//     user: 'User Name',
-//     avatar: 'https://www.w3schools.com/howto/img_avatar2.png',
-//     tweets: 247,
-//     followers: 100500,
-//     isFollowing: false,
-//   };
-
-//   componentDidMount() {
-//     const status = JSON.parse(localStorage.getItem('follow'));
-
-//     if (status === null) {
-//       localStorage.setItem('follow', JSON.stringify(this.state));
-//     } else if (status.isFollowing !== this.state.isFollowing) {
-//       this.setState({ ...status });
-//     }
-//   }
-
-//   componentDidUpdate(prevProps, prevState) {
-//     if (this.state.isFollowing !== prevState.isFollowing) {
-//       localStorage.setItem('follow', JSON.stringify(this.state));
-//     }
-//   }
-
-//   onFollow = () => {
-//     if (!this.state.isFollowing) {
-//       this.setState(prevState => ({
-//         followers: prevState.followers + 1,
-//         isFollowing: true,
-//       }));
-//     } else {
-//       this.setState(prevState => ({
-//         followers: prevState.followers - 1,
-//         isFollowing: false,
-//       }));
-//     }
-//   };
-
-//   render() {
-//     const { isFollowing, avatar, tweets, followers, user } = this.state;
-
-//     return (
-//       <Card
-//         avatar={avatar}
-//         tweets={tweets}
-//         followers={followers.toLocaleString('en')}
-//         user={user}
-//         isFollowing={isFollowing}
-//         onFollow={this.onFollow}
-//       />
-//     );
-//   }
-//================================================================================
 export class App extends Component {
   state = {
     data: users,
@@ -102,5 +46,4 @@ export class App extends Component {
       </Section>
     );
   }
-  //==================================================================================
 }
